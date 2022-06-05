3ee425ae33114560632613d027b5653db01a35d2
cimpal55 <cimpals71@gmail.com>
2022-06-05 22:49:20 +0300

    Programm start

---
+++ StipendijasKalkulators/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using StipendijasKalkulators.Core;
+
+namespace StipendijasKalkulators.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
